13db709834e95cad8911e8fc5e6861f18edaaed7
Ronald <ronald@haverman.com>
2018-11-21 19:16:56 +0000

    ... uitlegtekst..

---
+++ gbfs/importer.py
@@ -0,0 +1,22 @@
+import requests
+import cycle_location
+
+class GbfsImport():
+    def __init__(self, url,brand):
+        self.url = url
+        self.brand =brand
+
+    def import_bike(self, feed, bike):
+        return cycle_location.CycleLocation(feed + ':' + bike["bid"], bike["gLat"], bike["gLng"], feed, "bike", None)
+
+    def import_json(self, feed, json):
+        cycles = []
+        for bike in json["data"]:
+            cycles.append(self.import_bike(feed, bike))
+        return cycles
+
+    def import_feed(self):
+        cycles = []
+        r = requests.get(self.url)
+        return self.import_json(self.brand, r.json())
+
